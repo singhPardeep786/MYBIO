89152dc12fad9ec781ce934959b3699d32261134
singhPardeep786 <142139977+singhPardeep786@users.noreply.github.com>
2023-09-15 10:46:39 +0530

    Add files via upload

---
+++ Using BS5/script.js
@@ -0,0 +1,168 @@
+
+//-------RIGHT----CLICK---OFF-----//
+
+window.addEventListener("contextmenu", e => e.preventDefault());
+
+// ------------------------------------------------------------------------------------------------------//
+
+var typingElement = document.querySelector(".typing-text");
+var typeArray = ['Web Designer.', 'Graphic Designer.', 'Programmer.'];
+var index = 0,
+  isAdding = true,
+  typeIndex = 0;
+
+function playAnim() {
+  setTimeout(
+    function () {
+      typingElement.innerText = typeArray[typeIndex].slice(0, index);
+      /*
+        This line handles both typing and removing text
+        typeArray[typeIndex] selects the text from array
+        slice(0, index) selects the part of that text
+
+        Examples:
+
+        typeIndex = 0     => First Text
+        index = 0         => First Letter of the Text
+        Result is "H"
+
+        typeIndex = 0
+        index = 1
+        Result is "HT"    => First 2 Letters of the Text
+
+        typeIndex = 0
+        index = 2
+        Result is "HTM"   => First 3 Letters of the Text
+
+        typeIndex = 0
+        index = 3
+        Result is "HTML"  =>  Text typed completely, start to remove by decreasing index
+                              While removing, index will be: 2,1,0 . After 0, move on to next text. 
+
+        typeIndex = 1     => Second Text                   
+        index = 0
+        Result is "C"
+
+        typeIndex = 1
+        index = 1
+        Result is "CS"
+
+        typeIndex = 1
+        index = 2
+        Result is "CSS"
+      */
+
+      // If typing
+      if (isAdding) {
+        if (index >= typeArray[typeIndex].length) {
+          isAdding = false;
+          // If text typed completely, wait 2s before starting to remove it.
+          setTimeout(function () {
+            playAnim();
+          }, 2000);
+          return;
+        } else {
+          // Continue to typing text by increasing index
+          index++;
+        }
+      } else {
+        // If removing
+        if (index === 0) {
+          isAdding = true;
+          //If text removed completely, move on to next text by increasing typeIndex
+          typeIndex++;
+          if (typeIndex >= typeArray.length) {
+            // Turn to beginning when reached to last text
+            typeIndex = 0;
+          }
+        } else {
+          // Continue to removing text by decreasing index
+          index--;
+        }
+      }
+      // Call the function always
+      playAnim();
+    },
+
+    /* 
+      If typing text, call it every 120ms
+      If removing text, call it every 60ms
+      Type slower, remove faster
+    */
+    isAdding ? 80 : 20
+  );
+}
+
+// Start typing text
+playAnim();
+
+
+
+//--------back-to-top script START-------//
+
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+const pageProgressBar = document.querySelector(".progress-bar");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+document.addEventListener("scroll", () => {
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+backToTopButton.addEventListener("click", goToTop);
+
+
+//----------back-to-top script END---------//
+
+
+//----------GSAP---CODES--START-----------//
+
+gsap.from(".banner_animate", {
+  y: -100,
+  opacity: 0,
+  duration: 1.5,
+  delay: 1.5,
+})
+
+gsap.from(".svg_animate1", {
+  scale: 0,
+  opacity: 0,
+  duration: 1.5,
+  delay: 1.5,
+})
+
+
+var tl = gsap.timeline();
+
+tl.from(".nav_animation", {
+  y: -100,
+  duration: 1,
+  stagger: 0.2,
+  delay: 1.5,
+})
+
+//----------GSAP---CODES--END-----------//
+
